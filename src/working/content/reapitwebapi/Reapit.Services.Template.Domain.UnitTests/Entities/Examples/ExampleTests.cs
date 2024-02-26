a9f9ff10c6b23d878d6f6096bfc619a9c404fbfb
cduggan <cduggan@reapit.com>
2024-02-26 15:40:11 +0000

    test: add tests for domain project

---
+++ src/working/content/reapitwebapi/Reapit.Services.Template.Domain.UnitTests/Entities/Examples/ExampleTests.cs
@@ -0,0 +1,25 @@
+﻿using Reapit.Services.Template.Domain.Entities.Examples;
+using Reapit.Services.Template.Domain.Providers;
+
+namespace Reapit.Services.Template.Domain.UnitTests.Entities.Examples;
+
+public class ExampleTests
+{
+    private static readonly DateTimeOffset FixedDate = new(2000, 2, 14, 11, 52, 53, TimeSpan.FromHours(-7));
+    
+    // Create
+    
+    [Fact]
+    public void Create_PopulatesProperties_FromParameters()
+    {
+        const string name = "Name";
+        var date = new DateTime(1967, 9, 11);
+        using var context = new SystemTimeProviderContext(FixedDate);
+        
+        var example = Example.Create(name, date);
+        example.Name.Should().BeEquivalentTo(name);
+        example.Date.Should().Be(date);
+        example.Created.Should().Be(FixedDate);
+        example.Modified.Should().Be(FixedDate);
+    }
+}
---
+++ src/working/content/reapitwebapi/Reapit.Services.Template.Domain.UnitTests/Providers/SystemTimeProviderTests.cs
@@ -0,0 +1,18 @@
+﻿using Reapit.Services.Template.Domain.Providers;
+
+namespace Reapit.Services.Template.Domain.UnitTests.Providers;
+
+public class SystemTimeProviderTests
+{
+    [Fact]
+    public void Now_ReturnsNow_WhenNoContextConfiguredForScope()
+        => SystemTimeProvider.Now.Should().BeCloseTo(DateTimeOffset.Now, TimeSpan.FromSeconds(1));
+
+    [Fact]
+    public void Now_ReturnsConfiguredTime_WhenContextConfiguredForScope()
+    {
+        var fixedDate = new DateTimeOffset(2016, 4, 16, 7, 53, 12, TimeSpan.FromHours(-5));
+        using var timeContext = new SystemTimeProviderContext(fixedDate);
+        SystemTimeProvider.Now.Should().Be(fixedDate);
+    }
+}
