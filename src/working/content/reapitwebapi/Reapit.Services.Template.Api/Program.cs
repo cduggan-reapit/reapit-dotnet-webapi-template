9d3b4e0826f4e280c1778b6211d6a41569fde4c6
cduggan <cduggan@reapit.com>
2024-03-12 15:25:03 +0000

    refactor: move swagger configuration into its own extension class

--- src/working/content/reapitwebapi/Reapit.Services.Template.Api/Program.cs
+++ src/working/content/reapitwebapi/Reapit.Services.Template.Api/Program.cs
@@ -1,53 +1,22 @@
 using System.Reflection;
-using Microsoft.OpenApi.Models;
 using Reapit.Packages.ErrorHandling;
-using Reapit.Services.Template.Api.Controllers.Abstract;
 using Reapit.Services.Template.Api.Infrastructure.Swagger;
 using Reapit.Services.Template.Core;
-using Swashbuckle.AspNetCore.Filters;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddCoreServices();
-
-// Use controllers
-builder.Services.AddControllers()
-    .AddNewtonsoftJson();
+builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.RegisterErrorHandlerServices();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddSwaggerExamplesFromAssemblyOf<BaseController>();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(cfg =>
-{
-    cfg.SwaggerDoc("v1", new OpenApiInfo { Title = "Template API", Version = "v1" });
-    cfg.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Reapit.Services.Template.xml"));
-    
-    cfg.EnableAnnotations();
-    cfg.ExampleFilters();
-    cfg.DocumentFilter<JsonPatchDocumentFilter>();
-});
-
+builder.Services.AddConfiguredSwagger();
 
 var app = builder.Build();
 
 app.UseExceptionHandler(_ => { });
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseConfiguredSwagger();
 app.UseHttpsRedirection();
-
 app.MapControllers();
 app.UseRouting();
-
 
 app.Run();
