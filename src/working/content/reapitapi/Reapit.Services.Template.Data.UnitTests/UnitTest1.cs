31f159dc6a16108f66edd253b4feb286540c2948
cduggan <cduggan@reapit.com>
2024-03-14 12:23:10 +0000

    feat: add data layer to example-removed version

---
+++ src/working/content/reapitapi/Reapit.Services.Template.Data.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Reapit.Services.Template.Data.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/working/content/reapitapi/Reapit.Services.Template.Data/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Reapit.Services.Template.Data.Dynamo;
+
+namespace Reapit.Services.Template.Data;
+
+/// <summary>
+/// Extension methods for the configuration of data services
+/// </summary>
+public static class Startup
+{
+    /// <summary>
+    /// Configure services required for the data layer
+    /// </summary>
+    /// <param name="services">The service collection</param>
+    public static IServiceCollection AddDataServices(IServiceCollection services, IConfiguration configuration)
+    {
+        #if (isDynamoDataLayer)
+        
+        // Register IOptions<DynamoDbConfiguration>
+        services.Configure<DynamoDbConfiguration>(configuration.GetSection(key: DynamoDbConfiguration.Key));
+        // Register DynamoDbServices
+        
+        #elif (isMysqlDataLayer)
+        
+        // Register DbContext
+        
+        #endif
+        
+        return services;
+    }
+}
